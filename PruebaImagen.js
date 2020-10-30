3bd6bb9a945c6d78b27e5e53d26f7b84169a72b9
Sergio Ruiz <60074318+sergio8999@users.noreply.github.com>
2020-10-30 16:46:40 +0100

    Update PruebaImagen.js

--- PruebaImagen.js
+++ PruebaImagen.js
@@ -1,10 +1,5 @@
 function inicio() {
     var el = document.getElementsByTagName("img")[0];
-    el.addEventListener("touchstart", handleStart, false);
-    el.addEventListener("touchend", handleEnd, false);
-    //el.addEventListener("touchcancel", handleCancel, false);
-    //el.addEventListener("touchleave", handleLeave, false);
-    el.addEventListener("touchmove", handleMove, false);
     el.addEventListener('click', click);
 
     if (window.DeviceOrientationEvent){
@@ -18,21 +13,7 @@ function inicio() {
    * @param {DeviceOrientationEvent} e 
    */
   function orientacion(e){
-    document.getElementById('Imagen').style.transform = "rotateX("+e.beta-90+"deg); rotateY("+e.gamma+"deg); rotateZ("+e.alpha+"deg)";
-  }
-  function handleStart(evt) {
-    evt.preventDefault();
-    document.getElementById("log").innerHTML = "Touchstart " + evt.touches[0].pageX +" "+evt.touches[0].pageY ;
-  }
-
-  function handleEnd(evt) {
-    evt.preventDefault();
-    document.getElementById("log").innerHTML = "Touchend "+ evt.touches[0].pageX +" "+evt.touches[0].pageY;
-  }
-
-  function handleMove(evt) {
-    evt.preventDefault();
-    document.getElementById("log").innerHTML = "Touchmove "+ evt.touches[0].pageX +" "+evt.touches[0].pageY;
+    document.getElementById('Imagen').style.transform = "rotate("+e.alpha+"deg)";
   }
 
   function click(){
